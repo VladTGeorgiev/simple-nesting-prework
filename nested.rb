9460b930470562d62fff68c2c2c8df4f4f97472a
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-05-08 15:44:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -115,6 +115,7 @@ def adding_to_dennis
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-
+			}
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		 programmer_hash
 end
